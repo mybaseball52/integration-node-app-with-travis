1e700a2df3ad4208d8e0c1816190535281dd1c2e
mybaseball52 <mybaseball52@gmail.com>
2020-09-24 22:01:02 +0800

    first try

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const path = require('path');
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+const indexRouter = require("./routes/index");
+app.use("/", indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+module.exports = app;
